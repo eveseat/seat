c0bf82eaff2113d86037e4d6bef49095439ebd13
Leon Jacobs <leonja511@gmail.com>
2015-08-29 00:04:57 +0200

    Remove unused command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,6 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        \App\Console\Commands\Inspire::class,
     ];
 
     /**
@@ -24,7 +23,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
+
     }
 }
