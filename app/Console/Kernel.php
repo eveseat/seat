a56d3adf59e132bc6244cb358ea55f65835d08d7
Leon Jacobs <leonja511@gmail.com>
2015-08-29 14:00:40 +0200

    Schedule the eve:update-server-status command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,24 +5,33 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
+/**
+ * Class Kernel
+ * @package App\Console
+ */
 class Kernel extends ConsoleKernel
 {
+
     /**
      * The Artisan commands provided by your application.
      *
      * @var array
      */
     protected $commands = [
+        //
     ];
 
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     *
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
 
+        $schedule->command('eve:update-server-status')
+            ->everyFiveMinutes()->withoutOverlapping();
     }
 }
