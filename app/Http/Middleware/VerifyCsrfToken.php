4c0ea1e8965b72dd6f17bd3833cd8638f00a0cf2
Leon Jacobs <leonja511@gmail.com>
2015-11-29 09:40:10 +0200

    Bypass Csrf checks for API calls

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -6,12 +6,13 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
 {
+
     /**
      * The URIs that should be excluded from CSRF verification.
      *
      * @var array
      */
     protected $except = [
-        //
+        'api/*'
     ];
 }
