a712bfc19efddb23fcbe4c2c5761f4c43eba1419
Leon Jacobs <leonja511@gmail.com>
2015-12-02 16:50:10 +0200

    Set a default from

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'noreply@seat.local', 'name' => 'SeAT Administrator'],
 
     /*
     |--------------------------------------------------------------------------
