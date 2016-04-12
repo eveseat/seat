e8254d29bacbe46cce2105c57e2838daf0ecb285
Michael Ermer <michael.ermer@ermer.de>
2016-04-12 20:40:55 +0200

    add TrustedProxy to support nginx proxy with ssl before the setup, X-FORWARDED-PROTO

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \App\Http\Middleware\VerifyCsrfToken::class,
+        \Fideloper\Proxy\TrustProxies::class,
     ];
 
     /**
