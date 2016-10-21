2b9ade7f100e7612c6811c8abba08bb7fb53bfc6
Leon Jacobs <leonja511@gmail.com>
2016-10-21 20:56:17 +0200

    Disable `ONLY_FULL_GROUP_BY` constraint

--- config/database.php
+++ config/database.php
@@ -64,6 +64,20 @@ return [
             'prefix'    => '',
             'strict'    => true,
             'engine'    => null,
+            'modes'     => [
+                // For now, disable the full_group_by restriction.
+                // There are just too many queries not optimized
+                // for this, for now.
+                //'ONLY_FULL_GROUP_BY',
+
+                // All of the other modes though, they should stay!
+                'STRICT_TRANS_TABLES',
+                'NO_ZERO_IN_DATE',
+                'NO_ZERO_DATE',
+                'ERROR_FOR_DIVISION_BY_ZERO',
+                'NO_AUTO_CREATE_USER',
+                'NO_ENGINE_SUBSTITUTION'
+            ],
         ],
 
         'pgsql' => [
