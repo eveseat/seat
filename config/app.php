39ff75beb8185acd46f8490129e9178a9f190a70
Leon Jacobs <leonjza@users.noreply.github.com>
2018-04-29 18:48:57 +0200

    Apply fixes from StyleCI (#270)

--- config/app.php
+++ config/app.php
@@ -164,7 +164,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
-
         /*
          * SeAT Service Providers
          */
