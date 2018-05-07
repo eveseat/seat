2bd358461bf5ea7389a2bb2cb2eb4f1bf8522c8c
Leon Jacobs <leonja511@gmail.com>
2018-05-07 18:44:50 +0200

    Add TrustedProxies configuration

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The current proxy header mappings.
