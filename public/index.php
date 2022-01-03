139ebd15de1ae2e28408e5a3e78fed292c858b79
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2022-01-03 17:36:29 +0100

    style: apply styleCi changes

--- public/index.php
+++ public/index.php
@@ -9,8 +9,8 @@
  */
 define('LARAVEL_START', microtime(true));
 
-if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
-    require __DIR__.'/../storage/framework/maintenance.php';
+if (file_exists(__DIR__ . '/../storage/framework/maintenance.php')) {
+    require __DIR__ . '/../storage/framework/maintenance.php';
 }
 
 /*
