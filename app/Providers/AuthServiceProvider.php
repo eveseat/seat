0d4878fca249bcac6723a92277840a2878e115fd
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2022-02-08 23:29:42 +0100

    deps: update to L9

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,23 +7,12 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The policy mappings for the application.
-     *
-     * @var array
-     */
-    protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
-    ];
-
-    /**
      * Register any authentication / authorization services.
      *
      * @return void
      */
     public function boot()
     {
-
         $this->registerPolicies();
-
     }
 }
--- config/services.php
+++ config/services.php
@@ -8,15 +8,15 @@ return [
     |--------------------------------------------------------------------------
     |
     | This file is for storing the credentials for third party services such
-    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
-    | default location for this type of information, allowing packages
-    | to have a conventional place to find your various credentials.
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
     |
     */
 
     'mailgun' => [
-        'domain'   => env('MAILGUN_DOMAIN'),
-        'secret'   => env('MAILGUN_SECRET'),
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
 
@@ -25,18 +25,9 @@ return [
     ],
 
     'ses' => [
-        'key'    => env('SES_KEY'),
-        'secret' => env('SES_SECRET'),
-        'region' => 'us-east-1',
-    ],
-
-    // If you want to make use of SSO via EVEOnline, create
-    // an application at https://developers.eveonline.com/applications
-    // and fill in the ClientID and ClientSecret in the .env file
-    'eveonline' => [
-        'client_id'     => env('EVE_CLIENT_ID'),
-        'client_secret' => env('EVE_CLIENT_SECRET'),
-        'redirect'      => env('EVE_CALLBACK_URL'),
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
 ];
