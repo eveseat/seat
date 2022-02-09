6fb9cffae70274bc463cafb28355f7f6eadd4f34
Loïc Leuilliot <loic.leuilliot@gmail.com>
2022-02-09 20:58:53 +0100

    style: apply fixes from StyleCI (#859)
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -13,7 +13,7 @@ class Handler extends ExceptionHandler
      * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
-        //
+
     ];
 
     /**
@@ -35,7 +35,7 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+
         });
     }
 }
