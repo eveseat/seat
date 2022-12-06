15042bf92883e8f3d2680e66a6ecc1095962c995
Crypta-Eve <crypta@crypta.tech>
2022-12-06 23:30:04 +1030

    mysql db driver
    revert mysql to be the default db driver

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
