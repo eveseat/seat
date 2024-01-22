bb320377b36975af9863c71e01cfeb2b6a396724
Jay's <dallardj@gmail.com>
2024-01-22 02:36:05 +0100

    fix styling

--- config/app.php
+++ config/app.php
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'debug' => (bool)env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
